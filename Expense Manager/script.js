a27e5de45fdd631aad36ca3248a6a945fc7abe9e
Asteriqq <shayofunmifemi@gmail.com>
2023-12-06 13:28:43 +0100

    commit

--- Expense Manager/script.js
+++ Expense Manager/script.js
@@ -63,3 +63,82 @@ if (closingButton){
 /**Adding Expenses **/
 
 let button = document.getElementById('button')
+let DATE = document.getElementById('dateInput')
+let MERCHANT = document.getElementById('merchantInput')
+let TYPE = document.getElementById('type')
+let AMOUNT =  document.getElementById('amountInput')
+let DESCRIPTION = document.getElementById('descriptionInput')
+let STATE = document.getElementById('state')
+let FREQUENCY = document.getElementById('frequency')
+let table = document.getElementById('table')
+let expense_records = [];//array for local storage
+
+if(localStorage.getItem('expense_records')){
+    expense_records = JSON.parse(localStorage.getItem('expense_records'))
+    renderTable()
+}
+
+let inputs = [DATE, MERCHANT, AMOUNT, DESCRIPTION]//ARRAY FOR INPUTS
+
+function add_expense(date = 'N/A', merchant = 'N/A', type = 'N/A', amount = 'N/A', description = 'N/A', state = 'N/A', frequency = 'N/A' ){ 
+//default parameters
+
+let DATE_OBJECT = new Date(DATE.value)
+let FORMATTED_DATE = DATE_OBJECT.toLocaleDateString('en-US', {month: '2-digit', day: '2-digit', year: 'numeric'})
+
+date = FORMATTED_DATE
+merchant = MERCHANT.value
+type = TYPE.value
+amount = AMOUNT.value
+description = DESCRIPTION.value
+state = STATE.value
+frequency = FREQUENCY.value
+
+expense_records.push({date, merchant, type, amount, description, state, frequency})//add expense to array
+
+updateLocalStorage()
+
+renderTable();
+}
+
+//to delete an entry
+function delete_expense(index){
+    expense_records.splice(index,1)
+    updateLocalStorage()
+
+    renderTable()
+}
+
+function renderTable(){
+    table.innerHTML = `<tr>
+                        <th>Date</th>
+                        <th>Merchant</th>
+                        <th>Type</th>
+                        <th>Amount</th>
+                        <th>Decription</th>
+                        <th>State</th>
+                        <th>Frequency</th>
+                        <th>Action</th>
+
+                    </tr>`
+
+    expense_records.forEach((expense,index)=>{
+        table.innerHTML +=`<tr>
+                            <td>${expense.date}</td>
+                            <td>${expense.merchant}</td>
+                            <td>${expense.type}</td>
+                            <td>${expense.amount}</td>
+                            <td>${expense.description}</td>
+                            <td>${expense.state}</td>
+                            <td>${expense.frequency}</td>
+                            <td><button onclick = "delete_expense(${index})">Delete</button></td>
+                        </tr>`
+    })
+
+}
+
+function updateLocalStorage(){
+    localStorage.setItem('expense_records', JSON.stringify(expense_records));
+}
+
+button.addEventListener('click', add_expense)
